da087c2bfea40c31d1569980a36168e30ddd0a4a
NightFallF <nightfallzadh@gmail.com>
2025-01-28 20:40:14 +0530

    Add files via upload

---
+++ MatrixAddSub.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+public class MatrixAddSub {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = sc.nextInt();
+        System.out.print("\nEnter the number of columns: ");
+        int cols = sc.nextInt();
+
+        int[][] matrix1 = new int[rows][cols];
+        int[][] matrix2 = new int[rows][cols];
+        int[][] add = new int[rows][cols];
+        int[][] sub = new int[rows][cols];
+
+        System.out.println("\nEnter elements of the first matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix1[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("\nEnter elements of the second matrix:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix2[i][j] = sc.nextInt();
+            }
+        }
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                add[i][j] = matrix1[i][j] + matrix2[i][j];
+                sub[i][j] = matrix1[i][j] - matrix2[i][j];
+            }
+        }
+
+        System.out.println("\nAddition of Matrices:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(add[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\nSubtraction of Matrices:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(sub[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
